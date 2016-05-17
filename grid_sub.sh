b312a7a8a048b4b897e1d81cebdaa2bdda720d38
Brandon <broach@cern.ch>
2016-05-17 16:32:03 +0200

    removed incorrect option in grid_sub.sh

--- grid_sub.sh
+++ grid_sub.sh
@@ -1,1 +1,1 @@
-prun --exec ". ./grid_test.sh" --nJobs=3 --cmtConfig=x86_64-slc6-gcc49-opt --site=CERN-PROD --outDS user.broach.May17.gridtest.multi
+prun --exec ". ./grid_test.sh" --cmtConfig=x86_64-slc6-gcc49-opt --site=CERN-PROD --outDS user.broach.May17.gridtest.multi
